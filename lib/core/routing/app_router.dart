f134fcec664432af0a38f8351069630bbd60e1fb
ziad7118m <ziad7118m@gmail.com>
2026-05-19 18:58:57 +0300

    feat: add app routing setup

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:aurelia/core/routing/routes.dart';
+import 'package:aurelia/features/onboarding/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const Scaffold(body: Center(child: Text('Page not found'))),
+        );
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  Routes._();
+
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+}
