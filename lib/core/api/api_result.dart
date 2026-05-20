fd37de43385b9ce6f11b83b05e3d82bb08025d40
ziad7118m <ziad7118m@gmail.com>
2026-05-20 14:01:09 +0300

    chore: add api result wrapper

---
+++ lib/core/api/api_result.dart
@@ -0,0 +1,13 @@
+abstract class ApiResult<T> {
+  const ApiResult();
+}
+
+class ApiSuccess<T> extends ApiResult<T> {
+  final T data;
+  const ApiSuccess(this.data);
+}
+
+class ApiError<T> extends ApiResult<T> {
+  final String message;
+  const ApiError(this.message);
+}
